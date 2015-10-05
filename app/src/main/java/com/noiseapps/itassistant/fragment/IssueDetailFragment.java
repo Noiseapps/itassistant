bc25001a35aae6d28ce1f1d96ca8276ed2cbd99f
Tomek Ścibiorek <tomasz.scibiorek@exaco.pl>
2015-10-05 17:39:59 +0200

    issue details

--- app/src/main/java/com/noiseapps/itassistant/fragment/IssueDetailFragment.java
+++ app/src/main/java/com/noiseapps/itassistant/fragment/IssueDetailFragment.java
@@ -4,6 +4,8 @@ import android.support.v4.app.Fragment;
 import android.widget.TextView;
 
 import com.noiseapps.itassistant.R;
+import com.noiseapps.itassistant.model.jira.issues.Assignee;
+import com.noiseapps.itassistant.model.jira.issues.Fields;
 import com.noiseapps.itassistant.model.jira.issues.Issue;
 
 import org.androidannotations.annotations.AfterViews;
@@ -11,12 +13,13 @@ import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.FragmentArg;
 import org.androidannotations.annotations.OptionsItem;
 import org.androidannotations.annotations.ViewById;
+import org.apache.commons.lang3.StringUtils;
 
 @EFragment(R.layout.fragment_issue_detail)
 public class IssueDetailFragment extends Fragment {
 
     @ViewById
-    TextView textView2;
+    TextView type, priority, labels, status, issueName, assignee, reporter, description;
 
     @FragmentArg
     Issue issue;
@@ -24,7 +27,22 @@ public class IssueDetailFragment extends Fragment {
     @AfterViews
     void init() {
         setHasOptionsMenu(true);
-        textView2.setText(issue.toString());
+        fillTextViews();
+    }
+
+    private void fillTextViews() {
+        final Fields issueFields = issue.getFields();
+        type.setText(issueFields.getIssuetype().getName());
+        priority.setText(issueFields.getPriority().getName());
+        status.setText(issueFields.getStatus().getName());
+        labels.setText(StringUtils.join(issueFields.getLabels(), ", "));
+        final Assignee assignee = issueFields.getAssignee();
+        if(assignee != null) {
+            this.assignee.setText(assignee.getDisplayName());
+        }
+        reporter.setText(issueFields.getReporter().getDisplayName());
+        issueName.setText(String.format("%s (%s)", issueFields.getSummary(), issue.getKey()));
+        description.setText(issueFields.getDescription());
     }
 
     @OptionsItem(android.R.id.home)
