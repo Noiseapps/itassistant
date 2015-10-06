ed9505a587cbb57b74e73d059cf7e874a9f6dc28
Tomek Ścibiorek <tomasz.scibiorek@exaco.pl>
2015-10-06 18:36:50 +0200

    pre tab layout

--- app/src/main/java/com/noiseapps/itassistant/IssueDetailActivity.java
+++ app/src/main/java/com/noiseapps/itassistant/IssueDetailActivity.java
@@ -1,6 +1,9 @@
 package com.noiseapps.itassistant;
 
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.noiseapps.itassistant.fragment.IssueDetailFragment;
 import com.noiseapps.itassistant.fragment.IssueDetailFragment_;
@@ -9,6 +12,7 @@ import com.noiseapps.itassistant.model.jira.issues.Issue;
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Extra;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_issue_detail)
 public class IssueDetailActivity extends AppCompatActivity {
@@ -17,9 +21,24 @@ public class IssueDetailActivity extends AppCompatActivity {
 
     @AfterViews
     void init() {
+        initToolbar();
         final IssueDetailFragment fragment = IssueDetailFragment_.builder().issue(issue).build();
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.issue_detail_container, fragment)
                 .commit();
+    }
+
+    @ViewById(R.id.detail_toolbar)
+    Toolbar toolbar;
+
+    private void initToolbar() {
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white));
+        final ActionBar supportActionBar = getSupportActionBar();
+        if(supportActionBar != null) {
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+            supportActionBar.setHomeButtonEnabled(true);
+            supportActionBar.setTitle(String.format("%s (%s)", issue.getFields().getSummary(), issue.getKey()));
+        }
     }
 }
